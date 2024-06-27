25988645e8b4e23677f7552c8e3bc1ecd669d362
Pulkit <kpulkit15234@gmail.com>
2024-06-27 22:10:24 +0530

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import SmoothScrolling from "./components/SmoothScroll.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <SmoothScrolling>
     <BrowserRouter>
-      <App />
+      <>
+        <App />
+        <Analytics />
+      </>
     </BrowserRouter>
   </SmoothScrolling>
 );
